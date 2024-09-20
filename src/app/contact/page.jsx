623781003c65ddd3476807a650d8aa519362fba7
dabsmanuel <dabsmanuel5015@gmail.com>
2024-09-20 01:10:03 +0100

    added a contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -128,7 +128,7 @@ const Page = () => {
               type="submit"
               className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'
             >
-              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 pr-1'>Let's talk</span>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 pr-1'>Let&apos;s talk</span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]'/>
             </button>
           </motion.form>
