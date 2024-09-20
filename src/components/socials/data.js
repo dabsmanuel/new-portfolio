35bb8cbfdf316809aea7c81607f898912406f58e
dabsmanuel <dabsmanuel5015@gmail.com>
2024-09-20 17:41:40 +0100

    added links to the socials component

--- src/components/socials/data.js
+++ src/components/socials/data.js
@@ -3,35 +3,34 @@ import { BiLogoLinkedin} from "react-icons/bi";
 import { FaXTwitter } from "react-icons/fa6";
 
 
-
 export const SocialIcons = [
   {
     id: 1,
     icon: <RiWhatsappLine/>,
-    link:'#'
+    link:'https://wa.me/+2348158106602'
   },
 
   {
     id: 2,
     icon: <RiInstagramLine/>,
-    link:'#'
+    link:'https://instagram.com/dabs_manuel'
   },
 
   {
     id: 3,
     icon: <RiFacebookLine/>,
-    link:'#'
+    link:'https://facebook.com/dabsmanueloyibo?mibextid=ZbWKwL'
   },
 
   {
     id: 4,
     icon: <BiLogoLinkedin/>,
-    link:'#'
+    link:'https://linkedin.com/in/dabobuboibi-oyibo-a87717231'
   },
 
   {
     id: 5,
     icon: <FaXTwitter/>,
-    link:'#'
+    link:'https://x.com/dabs_manuel'
   },
 ]
